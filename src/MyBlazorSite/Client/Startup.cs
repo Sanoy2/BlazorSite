d402f65055a06c3c13acf86f9b564cc478a6480a
Krzysztof_Tomkow <218514@student.pwr.edu.pl>
2019-08-26 22:17:46 +0200

    Translation works!

--- src/MyBlazorSite/Client/Startup.cs
+++ src/MyBlazorSite/Client/Startup.cs
@@ -1,6 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using BlazorStrap;
 using Microsoft.AspNetCore.Components.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using Toolbelt.Blazor.Extensions.DependencyInjection;
 
 namespace MyBlazorSite.Client
 {
@@ -9,6 +14,7 @@ namespace MyBlazorSite.Client
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddBootstrapCSS();
+            services.AddI18nText<Startup>();
         }
 
         public void Configure(IComponentsApplicationBuilder app)
