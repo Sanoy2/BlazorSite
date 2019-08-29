554696cf7549205817dcb31251c5a2ff797acb6c
Krzysztof_Tomkow <218514@student.pwr.edu.pl>
2019-08-29 23:28:28 +0200

    Added language picker component

---
+++ src/MyBlazorSite/Client/Services/Settings/Settings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyBlazorSite.Client.Services.Settings
+{
+    public class Settings
+    {
+        public IEnumerable<string> SupportedLanguages { get; protected set; }
+
+        public Settings()
+        {
+            SupportedLanguages = new List<string>
+            {
+                "en",
+                "pl"
+            };
+        }
+    }
+}
